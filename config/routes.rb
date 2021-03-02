899fdc995d9c064376496e2199f3413434bc5f74
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-03-02 13:25:57 +0900

    add：Tweet投稿機能_(仮)完

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   resources :users, only: [:edit, :update]
-  resources :tweets, only: [:index, :new]
+  resources :tweets, only: [:index, :new, :create]
 end
