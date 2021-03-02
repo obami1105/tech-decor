f4f182d2ac9e53e5bec30672acf6b3b295ab732b
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-03-02 16:19:30 +0900

    add：Tweet詳細表示機能_(ﾙｰﾃｨﾝｸﾞ･ｺﾝﾄﾛｰﾗｰ定義まで)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,10 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  # def show
+  #   @tweet = Tweet.find(params[:id])
+  # end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
