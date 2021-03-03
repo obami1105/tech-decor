fe96256b7455bee1c362390f693dc0d13880a37f
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-03-03 13:11:20 +0900

    テスト2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_115051) do
+ActiveRecord::Schema.define(version: 2021_02_24_121032) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
