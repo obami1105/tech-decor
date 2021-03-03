8864446e0b09087b970cb7e0bab500c56aba989b
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-03-04 00:29:57 +0900

    撮影前_コミット

--- db/migrate/20210224121032_create_tweets.rb
+++ db/migrate/20210224121032_create_tweets.rb
@@ -1,7 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table   :tweets do |t|
-      t.string     :name,null: false
       t.string     :image
       t.text       :text
       t.references :user,null: false, foreign_key: :true
