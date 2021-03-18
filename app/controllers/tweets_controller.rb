c54e2f86006ebe86c516ba143033782985c8f89e
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-03-19 03:06:08 +0900

    add：編集ボタン ＆ ルーティング設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,11 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  # def edit
+  #   @tweet = Tweet.find(params[:id])
+  # end
+
+
   def show
     @tweet = Tweet.find(params[:id])
   end
