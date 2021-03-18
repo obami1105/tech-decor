fb65a7c3d1f03e824130955aa804e5f811d23ad8
obami1105 <75107822+obami1105@users.noreply.github.com>
2021-03-19 05:32:22 +0900

    add： 編集機能_(仮)完

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,9 +18,15 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
-  # def edit
-  #   @tweet = Tweet.find(params[:id])
-  # end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
 
 
   def show
